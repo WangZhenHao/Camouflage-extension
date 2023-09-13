2f9ad9997bd02c3af3db408a6d846ab5c5e8f6f7
wangzhenhao <1471047476@qq.com>
2023-09-13 23:13:06 +0800

    【feat】添加react框架

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import { Button } from 'antd';
+
+function App() {
+  return (
+    <div className="App">
+      <Button type="primary" style={{ marginLeft: '10px', marginTop: '10px' }}>Button</Button>
+    </div>
+  );
+}
+
+export default App;
