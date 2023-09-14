91468c52bd684b9e912d86e37b0685a54bab0fe8
udream-Wangzh <wangzh@udream.cn>
2023-09-14 15:51:21 +0800

    【fix】添加代码格式化

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import { Button } from 'antd';
+import { Button } from 'antd'
 function App() {
-  return (
-    <div className="App">
-       <Button type="primary" style={{ 'margin': "10px 0 0 10px" }}>你好</Button>
-    </div>
-  );
+    return (
+        <div className='App'>
+            <Button type='primary' style={{ margin: '10px 0 0 10px' }}>
+                你好
+            </Button>
+        </div>
+    )
 }
 
-export default App;
+export default App
